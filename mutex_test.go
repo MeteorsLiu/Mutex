0a5f5dfba2bd5a5f68b02671f8299a30cb4c850a
GuoHaoLan <coopticks@hotmail.com>
2022-11-19 16:14:24 +0800

    add test

--- mutex_test.go
+++ mutex_test.go
@@ -33,3 +33,38 @@ func TestMutex(t *testing.T) {
 	wg.Wait()
 	lock.Unlock()
 }
+
+func TestMuteX(t *testing.T) {
+	var lock Mutex
+	var wg sync.WaitGroup
+	wg.Add(5)
+	go func() {
+		defer wg.Done()
+		lock.Lock()
+		t.Log("Locked")
+	}()
+	go func() {
+		defer wg.Done()
+		lock.Lock()
+		t.Log("Locked 2")
+	}()
+	go func() {
+		defer wg.Done()
+		lock.Unlock()
+		t.Log("Unlocked 1")
+	}()
+	go func() {
+		defer wg.Done()
+		t.Log(lock.IsLocked())
+	}()
+
+	go func() {
+		defer wg.Done()
+		t.Log("is unlocked: ", lock.TryUnlock())
+	}()
+	wg.Wait()
+	if lock.IsLocked() {
+		t.Log("Unlock 2")
+		lock.Unlock()
+	}
+}
