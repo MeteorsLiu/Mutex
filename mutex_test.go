a4c478ff5efc1a449576514aade00f63880b2f08
GuoHaoLan <coopticks@hotmail.com>
2022-11-19 14:34:28 +0800

    add test

--- mutex_test.go
+++ mutex_test.go
@@ -1,29 +1,29 @@
 package mutex
 
 import (
-	"testing"
 	"sync"
+	"testing"
 )
 
 func TestMutex(t *testing.T) {
 	var lock Mutex
 	var wg sync.WaitGroup
 	wg.Add(4)
-	go func(){
+	go func() {
 		defer wg.Done()
 		lock.Lock()
 		t.Log("Locked")
 	}()
-	go func{
+	go func() {
 		defer wg.Done()
 		t.Log("is unlocked: ", lock.TryUnlock())
 	}()
-	go func{
+	go func() {
 		defer wg.Done()
 		t.Log(lock.IsLocked())
 	}()
 
-	go func{
+	go func() {
 		defer wg.Done()
 		t.Log("is unlocked: ", lock.TryUnlock())
 	}()
