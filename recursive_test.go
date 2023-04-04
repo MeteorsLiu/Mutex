8c5fad3896d11c11b195b1886ce98e8c2459a411
GuoHaoLan <coopticks@hotmail.com>
2023-04-04 17:53:26 +0800

    Add recursive lock

--- recursive_test.go
+++ recursive_test.go
@@ -3,38 +3,52 @@ package mutex
 import (
 	"sync"
 	"testing"
-	"time"
 )
 
 func TestRecursive(t *testing.T) {
 	var wg sync.WaitGroup
 	var rm Recursive
+	incr := 0
 	wg.Add(2)
 	go func() {
 		defer wg.Done()
-		defer rm.Unlock()
-		rm.Lock()
+
 		t.Log("1 Get the lock!")
 		for i := 0; i < 5; i++ {
-			if i == 3 {
-				t.Logf("1 unlock")
-				rm.Unlock()
-			}
 			rm.Lock()
-			t.Logf("1 Get the lock: %d!", i+1)
+			incr++
+			t.Logf("1 Get the lock: %d!", incr)
+		}
+		for i := 0; i < 5; i++ {
+			rm.Unlock()
 		}
 	}()
 	go func() {
 		defer wg.Done()
 		for i := 0; i < 5; i++ {
 			rm.Lock()
-			t.Log("2 Get the lock!")
+			incr++
+			t.Logf("2 Get the lock: %d!", incr)
+		}
+		for i := 0; i < 3; i++ {
 			rm.Unlock()
 		}
-		rm.Lock()
-		time.Sleep(time.Second)
-		t.Log("2 unlock")
-		rm.Unlock()
 	}()
+
+	/* Recursive Deadlock Example
+	go func() {
+	defer wg.Done()
+	for i := 0; i < 5; i++ {
+		rm.Lock()
+		incr++
+		t.Logf("2 Get the lock: %d!", incr)
+	}
+
+	for i := 0; i < 3; i++ {
+		rm.Unlock()
+	}
+	}()
+
+	*/
 	wg.Wait()
 }
